5c3ad125800ab3d616cefa3d9d2334d13b943345
BeshoyMorad <besh0morta@gmail.com>
2024-03-10 17:26:17 +0200

    Add patterns for job extractor service

--- services/job-extractor/api/index.py
+++ services/job-extractor/api/index.py
@@ -1,4 +1,6 @@
 from flask import request, jsonify, current_app as app
+import simplejson as json
+
 from .shared.helpers import (
     make_response_json,
 )
@@ -7,3 +9,10 @@ from .logger import logger
 def health_check():
     logger.debug("Health check")
     return "Hello World From Job Extractor Service!"
+
+def get_job_info(data):
+    logger.debug("Get Job Info for data: %s", data)
+    response = {
+        "data": "Beshoy"
+    }
+    return json.dumps(response)
