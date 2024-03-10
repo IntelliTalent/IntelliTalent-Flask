2f73873b4ae2ed5f9de4777bca22489112248e10
BeshoyMorad <besh0morta@gmail.com>
2024-03-10 17:26:10 +0200

    Add patterns for cv extractor service

--- services/cv-extractor/api/index.py
+++ services/cv-extractor/api/index.py
@@ -1,4 +1,6 @@
 from flask import request, jsonify, current_app as app
+import simplejson as json
+
 """from .profile.profile_service import (
     create_profile,
     get_all_profiles
@@ -11,3 +13,10 @@ from .logger import logger
 def health_check():
     logger.debug("Health check")
     return "Hello World From CV Extractor Service!"
+
+def get_cv_info(data):
+    logger.debug("Get CV Info for data: %s", data)
+    response = {
+        "data": "Beshoy"
+    }
+    return json.dumps(response)
