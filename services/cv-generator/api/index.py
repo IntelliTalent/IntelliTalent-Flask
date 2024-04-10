bc6c2a15f93fea08ab9290b329617840f9d589e2
MoazHassan2022 <moaz25jan2015@gmail.com>
2024-04-10 22:46:18 +0200

    finished preparing cv generator

--- services/cv-generator/api/index.py
+++ services/cv-generator/api/index.py
@@ -1,8 +1,4 @@
 from flask import request, jsonify, current_app as app
-"""from .profile.profile_service import (
-    create_profile,
-    get_all_profiles
-)"""
 from .shared.helpers import (
     make_response_json,
 )
@@ -16,33 +12,6 @@ def health_check():
 def generate_CV(data):
     logger.debug("Generating CV for data: %s", data)
     response = {
-        "pdf": "https://www.google.com",
-        "word": "https://www.google.com"
-    }
-    return json.dumps(response)
-
-def get_all_CVs(data):
-    logger.debug("Getting all CVs for data: %s", data)
-    response = {
-        "cvs": [
-            {
-                "pdf": "https://www.google.com",
-                "word": "https://www.google.com",
-                "profile_id": "1234"
-            },
-            {
-                "pdf": "https://www.google.com",
-                "word": "https://www.google.com",
-                "profile_id": "1235"
-            }
-        ]
-    }
-    return json.dumps(response)
-
-def get_profile_CV(data):
-    logger.debug("Getting CV for data: %s", data)
-    response = {
-        "pdf": "https://www.google.com",
         "word": "https://www.google.com"
     }
     return json.dumps(response)
